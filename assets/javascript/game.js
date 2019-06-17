1d7f2773588d146031408d33e59ef018c35b6903
Chen <chenx1@UTCCGL.com>
2019-06-16 21:07:10 -0700

    js edited

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
+
+
+
 var yourScore;
 var blueVal = Math.floor(Math.random() * 12) + 1;
 var greenVal = Math.floor(Math.random() * 12) + 1;
@@ -7,26 +10,35 @@ var yellowVal = Math.floor(Math.random() * 12) + 1;
 
 $("#green").on("click", function() {
   yourScore += greenVal;
+  $("#guess").text(yourScore);
 });
 $("#blue").on("click", function() {
   yourScore += blueVal;
+  $("#guess").text(yourScore);
 });
 $("#pink").on("click", function() {
   yourScore += pinkVal;
+  $("#guess").text(yourScore);
 });
 $("#yellow").on("click", function() {
   yourScore += yellowVal;
+  $("#guess").text(yourScore);
 });
-// add the value of the crystal to the score
+// add the value of the crystal to the score, and pop current guess to designated div
 
 var wins = 0;
 var loses = 0;
 
 var random = Math.floor(Math.random() * 102) + 19;
+$("#target").text(random);
 // generate the goal number;
 
 if (yourScore === random){
   wins++;
+  $("#win").text("win: "+ wins);
+  alert("you win!");
 } else if (yourScore === random){
-  loses++
+  loses++;
+  $("#lose").text("lose: "+ loses);
+  alert("you lose!");
 }
